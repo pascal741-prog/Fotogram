1ea787a443bfbf3102ee8f886165e8569f775d51
pascal741-prog <pascal8161@gmail.com>
2026-01-04 23:49:20 +0100

    dialog weiter angepasst / funktionen eingebaut

--- script.js
+++ script.js
@@ -1,50 +1,96 @@
-
-const OverlayRef = document.getElementById('overlay')
+const OverlayRef = document.getElementById("overlay");
 
 const imgFiles = [
-    './img/frozen lake.jpg',
-    './img/city-at-night.jpg',
-    './img/clouds.png',
-    './img/bird.jpg',
-    './img/hurrican.jpg',
-    './img/snow-mountain.jpg',
-    './img/duck.jpg',
-    './img/nightsky.jpg',
-    './img/bird2.jpg',
-    './img/leopards.jpg',
-    './img/winter-mountain.jpg',
-    './img/winter-tree.jpg',
+  "./img/frozen lake.jpg",
+  "./img/city-at-night.jpg",
+  "./img/clouds.png",
+  "./img/bird.jpg",
+  "./img/hurrican.jpg",
+  "./img/snow-mountain.jpg",
+  "./img/duck.jpg",
+  "./img/nightsky.jpg",
+  "./img/bird2.jpg",
+  "./img/leopards.jpg",
+  "./img/winter-mountain.jpg",
+  "./img/winter-tree.jpg",
 ];
 
 let currentIndex = 0;
+let totalPictures = 12;
 
-// open dialog  
+// open dialog
 
 function openOverlay(index) {
-    const OverlayRef = document.getElementById('overlay');
-    
-    currentIndex = index;
-    OverlayRef.showModal();
-    counter();
-    OpenCurrentPicture();
+  const OverlayRef = document.getElementById("overlay");
+
+  currentIndex = index;
+  OverlayRef.showModal();
+  counter();
+  OpenCurrentPicture();
+  countCurrentPicture();
 }
 
-// close Dialog 
+// close Dialog
 function closeOverlay() {
-  const OverlayRef = document.getElementById('overlay');
-  
-    OverlayRef.close();
+  const OverlayRef = document.getElementById("overlay");
+
+  OverlayRef.close();
 }
 
-function counter () {
-    console.log (currentIndex);
+function counter() {
+  console.log(currentIndex);
 }
 
-// open selected Photo 
+// open selected Photo
 
-function OpenCurrentPicture () {
-    const openPicture = document.getElementById("currentPicture");
-    
-    openPicture.src = imgFiles[currentIndex];
+function OpenCurrentPicture() {
+  const openPicture = document.getElementById("currentPicture");
 
+  openPicture.src = imgFiles[currentIndex];
+}
+
+// Picture Counter
+
+function countCurrentPicture() {
+  const counter = document.getElementById("counter");
+  counter.textContent = `${currentIndex + 1} / ${imgFiles.length}`;
+}
+
+// switch picture left
+
+function switchSelectedPhotoLeft() {
+  if (currentIndex > 0) {
+    currentIndex--;
+  }
+  else{
+    currentIndex = imgFiles.length -1;
+  }
+  OpenCurrentPicture();
+  countCurrentPicture();
+}
+
+// switch picture right 
+
+function switchSelectedPhotoRight() {
+  if (currentIndex < imgFiles.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  OpenCurrentPicture();
+  countCurrentPicture();
+}
+
+// show img description
+
+function showDescription() {
+  if (currentIndex > 0) {
+    currentIndex--;
+  }
+  else{
+    currentIndex = imgFiles.length -1;
+  }
+  OpenCurrentPicture();
+  countCurrentPicture();
+  showDescription();
 }
